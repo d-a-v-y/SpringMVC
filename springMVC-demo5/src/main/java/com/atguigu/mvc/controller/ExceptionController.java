b81b19175a2c614dee4da15289906a1cbdbe2587
Davy <2689022556@qq.com>
2022-05-08 19:41:11 +0800

    2022_5_8 commit

---
+++ springMVC-demo5/src/main/java/com/atguigu/mvc/controller/ExceptionController.java
@@ -0,0 +1,14 @@
+package com.atguigu.mvc.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/**
+ * @author Davy
+ */
+
+@ControllerAdvice
+public class ExceptionController {
+
+//    @ExceptionHandler(value = {Ar})
+}
