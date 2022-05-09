c0f9ae3cfdf02ae5e01a5c60e3a2613d7ba52b7b
Davy <2689022556@qq.com>
2022-05-09 13:42:05 +0800

    2022_5_9 commit

---
+++ springMVC-annotation/src/main/java/com/atguigu/mvc/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.atguigu.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Davy
+ */
+
+@Controller
+public class TestController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}
